9c1d20e033b857aa68b79a41ac2f706221edce06
= <=>
2024-03-19 18:17:40 +0530

    first commit

---
+++ Javascript codes/project_js/general.js
@@ -0,0 +1,11 @@
+const emptyObject = {}
+
+if(Object.keys(emptyObject).length == 0){
+    console.log("'''object is empty'''");
+}
+
+const num = 100
+
+num < 80 ? console.log("less") : console.log("more")  ;
+
+
